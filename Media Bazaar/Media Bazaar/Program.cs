12a7659000f3f94371f21653b50009777ce64c63
Catalin Mihai Popoiu <500157@student.fontys.nl>
2023-09-26 11:40:21 +0200

    Added UML, started adding forms (WIP)

--- Media Bazaar/Media Bazaar/Program.cs
+++ Media Bazaar/Media Bazaar/Program.cs
@@ -16,7 +16,7 @@ namespace Media_Bazaar
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
