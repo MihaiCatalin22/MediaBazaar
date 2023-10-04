a4b9918c2e52e9cb38884188171fa4255cdb8bff
Murat Zunbul <i464839@fhict.nl>
2023-10-04 11:13:22 +0200

    AdminEmployee new updated

--- New Media Bazaar/Desktop application/Desktop application/EmployeeAddForm.cs
+++ New Media Bazaar/Desktop application/Desktop application/EmployeeAddForm.cs
@@ -6,8 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-using 
+using System.Windows.Forms; 
 
 namespace Desktop_application
 {
