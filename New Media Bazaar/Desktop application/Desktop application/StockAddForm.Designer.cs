4d21c78b964ef11bb47c701200afef969b45da06
Murat Zunbul <i464839@fhict.nl>
2023-12-20 15:49:38 +0100

    StockAddForm design completed

--- New Media Bazaar/Desktop application/Desktop application/StockAddForm.Designer.cs
+++ New Media Bazaar/Desktop application/Desktop application/StockAddForm.Designer.cs
@@ -1,165 +1,196 @@
 ﻿namespace Desktop_application
 {
-	partial class StockAddForm
-	{
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+    partial class StockAddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			btnBack = new Button();
-			btnAdd = new Button();
-			lblVrajeala = new Label();
-			lblName = new Label();
-			lblQuantity = new Label();
-			lblType = new Label();
-			cbType = new ComboBox();
-			tbName = new TextBox();
-			NUDQuantity = new NumericUpDown();
-			((System.ComponentModel.ISupportInitialize)NUDQuantity).BeginInit();
-			SuspendLayout();
-			// 
-			// btnBack
-			// 
-			btnBack.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-			btnBack.Location = new Point(79, 288);
-			btnBack.Name = "btnBack";
-			btnBack.Size = new Size(118, 48);
-			btnBack.TabIndex = 0;
-			btnBack.Text = "Go back";
-			btnBack.UseVisualStyleBackColor = true;
-			btnBack.Click += btnBack_Click;
-			// 
-			// btnAdd
-			// 
-			btnAdd.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-			btnAdd.Location = new Point(229, 288);
-			btnAdd.Name = "btnAdd";
-			btnAdd.Size = new Size(118, 48);
-			btnAdd.TabIndex = 1;
-			btnAdd.Text = "Add new stock";
-			btnAdd.UseVisualStyleBackColor = true;
-			btnAdd.Click += btnAdd_Click;
-			// 
-			// lblVrajeala
-			// 
-			lblVrajeala.AutoSize = true;
-			lblVrajeala.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-			lblVrajeala.Location = new Point(139, 21);
-			lblVrajeala.Name = "lblVrajeala";
-			lblVrajeala.Size = new Size(134, 21);
-			lblVrajeala.TabIndex = 2;
-			lblVrajeala.Text = "Add a new stock";
-			// 
-			// lblName
-			// 
-			lblName.AutoSize = true;
-			lblName.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-			lblName.Location = new Point(39, 117);
-			lblName.Name = "lblName";
-			lblName.Size = new Size(103, 21);
-			lblName.TabIndex = 30;
-			lblName.Text = "Stock name:";
-			// 
-			// lblQuantity
-			// 
-			lblQuantity.AutoSize = true;
-			lblQuantity.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-			lblQuantity.Location = new Point(61, 151);
-			lblQuantity.Name = "lblQuantity";
-			lblQuantity.Size = new Size(81, 21);
-			lblQuantity.TabIndex = 29;
-			lblQuantity.Text = "Quantity:";
-			// 
-			// lblType
-			// 
-			lblType.AutoSize = true;
-			lblType.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-			lblType.Location = new Point(48, 83);
-			lblType.Name = "lblType";
-			lblType.Size = new Size(94, 21);
-			lblType.TabIndex = 28;
-			lblType.Text = "Stock type:";
-			// 
-			// cbType
-			// 
-			cbType.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-			cbType.FormattingEnabled = true;
-			cbType.Location = new Point(152, 83);
-			cbType.Name = "cbType";
-			cbType.Size = new Size(152, 28);
-			cbType.TabIndex = 31;
-			// 
-			// tbName
-			// 
-			tbName.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-			tbName.Location = new Point(152, 117);
-			tbName.Name = "tbName";
-			tbName.Size = new Size(152, 27);
-			tbName.TabIndex = 32;
-			// 
-			// NUDQuantity
-			// 
-			NUDQuantity.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-			NUDQuantity.Location = new Point(152, 150);
-			NUDQuantity.Name = "NUDQuantity";
-			NUDQuantity.Size = new Size(151, 27);
-			NUDQuantity.TabIndex = 33;
-			// 
-			// StockAddForm
-			// 
-			AutoScaleDimensions = new SizeF(7F, 15F);
-			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(422, 374);
-			Controls.Add(NUDQuantity);
-			Controls.Add(tbName);
-			Controls.Add(cbType);
-			Controls.Add(lblName);
-			Controls.Add(lblQuantity);
-			Controls.Add(lblType);
-			Controls.Add(lblVrajeala);
-			Controls.Add(btnAdd);
-			Controls.Add(btnBack);
-			FormBorderStyle = FormBorderStyle.FixedToolWindow;
-			Name = "StockAddForm";
-			StartPosition = FormStartPosition.CenterScreen;
-			Text = "Add new stocks";
-			((System.ComponentModel.ISupportInitialize)NUDQuantity).EndInit();
-			ResumeLayout(false);
-			PerformLayout();
-		}
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StockAddForm));
+            btnBack = new Button();
+            btnAdd = new Button();
+            lblVrajeala = new Label();
+            lblName = new Label();
+            lblQuantity = new Label();
+            lblType = new Label();
+            cbType = new ComboBox();
+            tbName = new TextBox();
+            NUDQuantity = new NumericUpDown();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)NUDQuantity).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // btnBack
+            // 
+            btnBack.BackColor = Color.Firebrick;
+            btnBack.FlatAppearance.BorderSize = 0;
+            btnBack.FlatStyle = FlatStyle.Flat;
+            btnBack.Font = new Font("Century Gothic", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnBack.ForeColor = Color.White;
+            btnBack.Location = new Point(426, -1);
+            btnBack.Margin = new Padding(3, 4, 3, 4);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(58, 38);
+            btnBack.TabIndex = 0;
+            btnBack.Text = "X";
+            btnBack.UseVisualStyleBackColor = false;
+            btnBack.Click += btnBack_Click;
+            // 
+            // btnAdd
+            // 
+            btnAdd.BackColor = Color.MediumSeaGreen;
+            btnAdd.FlatAppearance.BorderSize = 0;
+            btnAdd.FlatStyle = FlatStyle.Flat;
+            btnAdd.Font = new Font("Century Gothic", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            btnAdd.ForeColor = Color.White;
+            btnAdd.Location = new Point(89, 295);
+            btnAdd.Margin = new Padding(3, 4, 3, 4);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(315, 64);
+            btnAdd.TabIndex = 1;
+            btnAdd.Text = "Add new stock";
+            btnAdd.UseVisualStyleBackColor = false;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // lblVrajeala
+            // 
+            lblVrajeala.AutoSize = true;
+            lblVrajeala.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblVrajeala.Location = new Point(150, 48);
+            lblVrajeala.Name = "lblVrajeala";
+            lblVrajeala.Size = new Size(168, 28);
+            lblVrajeala.TabIndex = 2;
+            lblVrajeala.Text = "Add a new stock";
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblName.Location = new Point(79, 159);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(127, 28);
+            lblName.TabIndex = 30;
+            lblName.Text = "Stock name:";
+            // 
+            // lblQuantity
+            // 
+            lblQuantity.AutoSize = true;
+            lblQuantity.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblQuantity.Location = new Point(104, 204);
+            lblQuantity.Name = "lblQuantity";
+            lblQuantity.Size = new Size(100, 28);
+            lblQuantity.TabIndex = 29;
+            lblQuantity.Text = "Quantity:";
+            // 
+            // lblType
+            // 
+            lblType.AutoSize = true;
+            lblType.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblType.Location = new Point(89, 114);
+            lblType.Name = "lblType";
+            lblType.Size = new Size(117, 28);
+            lblType.TabIndex = 28;
+            lblType.Text = "Stock type:";
+            // 
+            // cbType
+            // 
+            cbType.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            cbType.FormattingEnabled = true;
+            cbType.Location = new Point(208, 114);
+            cbType.Margin = new Padding(3, 4, 3, 4);
+            cbType.Name = "cbType";
+            cbType.Size = new Size(173, 33);
+            cbType.TabIndex = 31;
+            // 
+            // tbName
+            // 
+            tbName.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            tbName.Location = new Point(208, 159);
+            tbName.Margin = new Padding(3, 4, 3, 4);
+            tbName.Name = "tbName";
+            tbName.Size = new Size(173, 32);
+            tbName.TabIndex = 32;
+            // 
+            // NUDQuantity
+            // 
+            NUDQuantity.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            NUDQuantity.Location = new Point(208, 203);
+            NUDQuantity.Margin = new Padding(3, 4, 3, 4);
+            NUDQuantity.Name = "NUDQuantity";
+            NUDQuantity.Size = new Size(173, 32);
+            NUDQuantity.TabIndex = 33;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(125, 62);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 34;
+            pictureBox1.TabStop = false;
+            // 
+            // StockAddForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(482, 430);
+            Controls.Add(pictureBox1);
+            Controls.Add(NUDQuantity);
+            Controls.Add(tbName);
+            Controls.Add(cbType);
+            Controls.Add(lblName);
+            Controls.Add(lblQuantity);
+            Controls.Add(lblType);
+            Controls.Add(lblVrajeala);
+            Controls.Add(btnAdd);
+            Controls.Add(btnBack);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "StockAddForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Add new stocks";
+            ((System.ComponentModel.ISupportInitialize)NUDQuantity).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
+        #endregion
 
-		private Button btnBack;
-		private Button btnAdd;
-		private Label lblVrajeala;
-		private Label lblName;
-		private Label lblQuantity;
-		private Label lblType;
-		private ComboBox cbType;
-		private TextBox tbName;
-		private NumericUpDown NUDQuantity;
-	}
+        private Button btnBack;
+        private Button btnAdd;
+        private Label lblVrajeala;
+        private Label lblName;
+        private Label lblQuantity;
+        private Label lblType;
+        private ComboBox cbType;
+        private TextBox tbName;
+        private NumericUpDown NUDQuantity;
+        private PictureBox pictureBox1;
+    }
 }
--- New Media Bazaar/Desktop application/Desktop application/StockAddForm.cs
+++ New Media Bazaar/Desktop application/Desktop application/StockAddForm.cs
@@ -13,60 +13,60 @@ using Logic.Controllers;
 
 namespace Desktop_application
 {
-	public partial class StockAddForm : Form
-	{
-		private Employee _loggedInEmployee;
-		private RestockRequestController _stockController;
-		public StockAddForm(Employee loggedInEmployee)
-		{
-			InitializeComponent();
+    public partial class StockAddForm : Form
+    {
+        private Employee _loggedInEmployee;
+        private RestockRequestController _stockController;
+        public StockAddForm(Employee loggedInEmployee)
+        {
+            InitializeComponent();
 
-			_loggedInEmployee = loggedInEmployee;
-			_stockController = new RestockRequestController(new DALRestockRequestsController());
+            _loggedInEmployee = loggedInEmployee;
+            _stockController = new RestockRequestController(new DALRestockRequestsController());
 
-			PopulateStockType();
-		}
-		private void PopulateStockType()
-		{
-			cbType.DataSource = Enum.GetValues(typeof(StockType));
-		}
-		private void btnBack_Click(object sender, EventArgs e)
-		{
-			AdminStockDetails form = new AdminStockDetails(_loggedInEmployee);
-			this.Hide();
-			form.ShowDialog();
-			this.Close();
-		}
+            PopulateStockType();
+        }
+        private void PopulateStockType()
+        {
+            cbType.DataSource = Enum.GetValues(typeof(StockType));
+        }
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            AdminStockDetails form = new AdminStockDetails(_loggedInEmployee);
+            this.Hide();
+            form.ShowDialog();
+            this.Close();
+        }
 
-		private void btnAdd_Click(object sender, EventArgs e)
-		{
-			string stockName = tbName.Text;
-			StockType stockType = (StockType)cbType.SelectedItem;
-			int quantity = (int)NUDQuantity.Value;
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string stockName = tbName.Text;
+            StockType stockType = (StockType)cbType.SelectedItem;
+            int quantity = (int)NUDQuantity.Value;
 
-			if (string.IsNullOrWhiteSpace(stockName))
-			{
-				MessageBox.Show("Please enter a valid stock name");
-			}
+            if (string.IsNullOrWhiteSpace(stockName))
+            {
+                MessageBox.Show("Please enter a valid stock name");
+            }
 
-			bool success = _stockController.AddNewStock(stockName, stockType, quantity);
+            bool success = _stockController.AddNewStock(stockName, stockType, quantity);
 
-			if (success)
-			{
-				MessageBox.Show("The new stock item was added successfully into the system.");
-				ClearFormInputs();
-			}
-			else
-			{
-				MessageBox.Show("Failed to add the stock item to the system.");
-			}
-		}
+            if (success)
+            {
+                MessageBox.Show("The new stock item was added successfully into the system.");
+                ClearFormInputs();
+            }
+            else
+            {
+                MessageBox.Show("Failed to add the stock item to the system.");
+            }
+        }
 
-		private void ClearFormInputs()
-		{
-			tbName.Clear();
-			NUDQuantity.Value = NUDQuantity.Minimum;
-			cbType.SelectedIndex = 0;
-		}
-	}
+        private void ClearFormInputs()
+        {
+            tbName.Clear();
+            NUDQuantity.Value = NUDQuantity.Minimum;
+            cbType.SelectedIndex = 0;
+        }
+    }
 }
